4a59f049794f32f66e84b55224519a0006f77c4e
Carwest Song <carwestsam@gmail.com>
2014-11-22 14:00:32 +0800

    console log contianer with html tags

--- Lesson 2/disklist.js
+++ Lesson 2/disklist.js
@@ -3,10 +3,29 @@
  * Copyright (C) 2014 carwest <carwest@carwest-UDISK>
  *
  * Distributed under terms of the MIT license.
- */
+ */ 
 
 $(document).ready(function(){
     var container = $('#contianer');
-    console.log( container );
+    console.log( diskDiv( disks[0] ) )
 });
 
+function diskDiv(disk){
+    return $("<div>")
+        .attr("class", "disk1")
+        .append(diskTitle(disk.name))
+        .append(diskImg(disk.img))
+        .append(diskDesc(disk.desc));
+}
+
+function diskTitle(title){
+    return $("<h3>").html(title);
+}
+
+function diskImg(img) {
+    return $("<img>").attr("src", img);
+}
+
+function diskDesc(desc){
+    return $("<p>").html(desc);
+}
