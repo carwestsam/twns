6fece73d8b3bc3c87f18f488bad58744dbd9d119
Carwest Song <carwestsam@gmail.com>
2014-11-22 13:43:05 +0800

    console log container

---
+++ Lesson 2/disklist.js
@@ -0,0 +1,12 @@
+/*
+ * disklist.js
+ * Copyright (C) 2014 carwest <carwest@carwest-UDISK>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+$(document).ready(function(){
+    var container = $('#contianer');
+    console.log( container );
+});
+
