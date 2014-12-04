47bbcddc6153069338dcf041a8161d327e95c665
Carwest Song <carwestsam@gmail.com>
2014-12-04 20:01:35 +0800

    继续调整卡片的展现方式，添加载入fakedata的方法

--- twns_tmp/fakedata.js~
+++ twns_tmp/fakedata.js~
@@ -10,16 +10,19 @@ var disks = [
 {
     name:"CZ80",
     img:"/pics/cz80.png",
-    desc:"至尊极速 318元"
+    desc:"至尊极速",
+    price: "318元"
 },
 {
     name:"MAXELL CD-R",
     img:"/pics/disk.png",
-    desc:"最美的声音 50元/5张"
+    desc:"最美的声音",
+    price:"5元"
 },
 {
     name:"500GB 硬盘",
     img:"/pics/harddisk2.png",
-    desc:"最轻薄的硬盘 355元"
+    desc:"最轻薄的硬盘",
+    price:"355元"
 }];
 
--- twns_tmp/js/modules/itemlist.js
+++ twns_tmp/js/modules/itemlist.js
@@ -20,27 +20,43 @@ define( ["jquery", "underscore"], function($, _){
             $("#itemlist").append( div );
         } );
         $("#itemlist").children().each( function(){
-            $(this).height ( $(this).width() / 0.618 + 'px');
-        } )
+           //$(this).height ( $(this).width() / 0.744 + 'px');
+        } );
     };
 
     function diskDiv( disk ){
         return $("<div>")
             .attr( "class", "item" )
-            .append( diskTitle(disk.name) )
-            .append( diskImg( disk.img ) )
-            .append( diskDesc(disk.desc) );
+            .append( diskImg(disk.img) )
+            .append( diskNote( disk.name, disk.desc, disk.price ) );
     };
 
-    function diskTitle( title ){
-        return $("<h4>").html(title);
-    };
-    function diskImg(img){
+    function diskImg( img ){
         return $("<img>").attr("src", img);
+    }
+    function diskNote( name, desc, price ){
+        return $('<div>')
+            .attr( 'class', 'note' )
+            .append( diskTitle( name ) )
+            .append( diskDesc( desc ) )
+            .append( diskPrice( price ) );
+    }
+
+    function diskTitle( title ){
+        return $("<div>")
+            .attr( 'class', 'title' )
+            .html(title);
     };
     function diskDesc( desc ){
-        return $("<p>").html(desc);
-    }
+        return $("<div>")
+            .attr('class', 'desc')
+            .html(desc);
+    };
+    function diskPrice( price ){
+        return $("<div>")
+            .attr('class', 'price')
+            .html(price);
+    };
 } );
 
 
